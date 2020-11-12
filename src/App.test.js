804176ac5e5cf2ecb302f234778215816cd0fc3e
Salman <m.salmank9991@gmail.com>
2020-11-12 09:13:43 -0800

    added

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,8 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+    const element=render(<App />);
+    console.log("element =",element)
+  const linkElement = element.getByText(/hello world/i);
   expect(linkElement).toBeInTheDocument();
 });
